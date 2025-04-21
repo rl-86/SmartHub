be740806299bf3bf4a996ae2df9cf6ef3452ef8d
rl-86 <rl86@live.se>
2025-04-21 16:46:50 +0200

    Initial commit of the backend

---
+++ src/main/java/org/smarthub/backend/SmarthubApplication.java
@@ -0,0 +1,13 @@
+package org.smarthub.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarthubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmarthubApplication.class, args);
+    }
+
+}
